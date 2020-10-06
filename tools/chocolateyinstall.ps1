d7f712ddde9e3752d05fa4d87a0c339b280332de
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2020-10-06 09:47:09 +0200

    Update to version 4.7

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64      = 'https://github.com/FreeFem/FreeFem-sources/releases/download/v4.6/FreeFEM-4.6-win7-64.exe'
+$url64 = 'https://github.com/FreeFem/FreeFem-sources/releases/download/v4.7/FreeFEM-4.7-win7-64.exe'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url64bit      = $url64
-  softwareName  = 'freefem*'
-  checksum64    = '41C9D562AD5A834B2D3B71F4692C6CF3FA16D3B12B71D7D77593CABEF9B74E20'
-  checksumType64= 'sha256'
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  validExitCodes= @(0)
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'EXE'
+  url64bit       = $url64
+  softwareName   = 'freefem*'
+  checksum64     = 'e614708292cec6d888ca8d7d4afd5dede448a015da36fa116165e23904dac8f5'
+  checksumType64 = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
