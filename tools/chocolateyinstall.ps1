3c8cfc001e34adb8249ab1bccf6ebba0d8c336a8
rogier5 <nicolas.rogier@univ-lorraine.fr>
2020-10-18 16:16:43 +0200

    update to version 4.7.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64 = 'https://github.com/FreeFem/FreeFem-sources/releases/download/v4.7/FreeFEM-4.7-win7-64.exe'
+$url64 = 'https://github.com/FreeFem/FreeFem-sources/releases/download/v4.7-1/FreeFEM-4.7-1-win7-64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType       = 'EXE'
   url64bit       = $url64
   softwareName   = 'freefem*'
-  checksum64     = 'e614708292cec6d888ca8d7d4afd5dede448a015da36fa116165e23904dac8f5'
+  checksum64     = '1e4934d93d806405484579ff665e0c056ea9b9827ea79a417c7019436a5333de'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
--- update.ps1
+++ update.ps1
@@ -7,9 +7,9 @@ $latestrelease = 'https://github.com/FreeFem/FreeFem-sources/releases/latest'
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^[$]url64\s*=\s*)('.*')"              = "`$1'$($Latest.URL64)'"
-            "(^\s*checksum64\s*=\s*)('.*')"         = "`$1'$($Latest.Checksum64)'"
-            "(^\s*checksumType64\s*=\s*)('.*')"     = "`$1'$($Latest.ChecksumType64)'"
+            "(^[$]url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
+            "(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
 }
@@ -18,12 +18,14 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $latestrelease -UseBasicParsing
     $regex = 'FreeFEM-.*-win7-64\.exe$'
     $url = ($download_page.Links | Where-Object href -match $regex | Select-Object -First 1 -ExpandProperty href) -split '/', 5 | Select-Object -Last 1
-    $version = Get-Version $url
+    
+    $url -match '\/v(?<version>.*)\/'
+    $version = $Matches.version -replace '-', '.'
 
     $releases = $latestrelease.Substring(0, $latestrelease.LastIndexOf('/'))
     return @{
-        Version          = $version
-        URL64              = "$releases/$url"
+        Version = $version
+        URL64   = "$releases/$url"
     }
 
 }
