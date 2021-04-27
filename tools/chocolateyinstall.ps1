3833761683e16b4a518738eb766f8f1821cd6c7e
ROGIER Nicolas <nicolas.rogier@univ-lorraine.fr>
2021-04-27 08:43:44 +0200

    update to version 4.9

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url64 = 'https://github.com/FreeFem/FreeFem-sources/releases/download/v4.8/FreeFEM-4.8-win7-64.exe'
+$url64 = 'https://github.com/FreeFem/FreeFem-sources/releases/download/v4.9/FreeFEM-4.9-win7-64.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType       = 'EXE'
   url64bit       = $url64
   softwareName   = 'freefem*'
-  checksum64     = '517a952a02f223e785ed207c29bf7a0d2cbf692ac5eaaeff3e949c345c930eed'
+  checksum64     = '63f45a976c0af4e98aed0cd82ed0611dfe473364530280c201f053b0d4ee3969'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes = @(0)
